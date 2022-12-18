d9580333aeaccb8b53d0e8f47a7f3bc6af614bf1
Daniel Jimenez <daniel.jimenez.cr@gmail.com>
2022-12-18 18:15:03 +0000

    add files to repository

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,56 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+
+	const [toDos, setTodos] = useState([])
+	const [inputTask, setInputTask] = useState("")
+
+	function addNewTask(e) {
+		if (e.key === "Enter" && inputTask != "") {
+			setTodos(toDos.concat([inputTask]));
+			setInputTask("");
+		}
+	}
+
+	function deleteTask(i) {
+		setTodos(toDos.filter((i, selectedIndex) => index != selectedIndex))
+	}
+
+	function deleteTask(i) {
+		let newTodos = [...toDos]
+		newTodos.splice(i, 1)
+		setTodos(newTodos)
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+		<div className="container main-container">
+			<div className="todo-container">
+				<h1 className="text-center app-title"> <i className="fa-solid fa-circle-check"></i> to-do list</h1>
+				<div className="input-container">
+					<input
+						className="input-task"
+						type="text"
+						onChange={(e) => setInputTask(e.target.value)}
+						value={inputTask}
+						onKeyDown={addNewTask}
+						placeholder="Add your new task" />
+
+				</div>
+				<ul className="task-list">
+
+					{toDos.map((task) => (
+						<li className="task-item">
+							{task}
+
+							<i onClick={deleteTask} class="fa-regular fa-trash-can trash-icon"> </i>
+						</li>
+
+					))}
+				</ul>
+				
+			</div>
 		</div>
 	);
 };
